1ecd4a14e065d685d28cb0da2601a00606392c9c
Alexander Dümont <alexander_duemont@web.de>
2024-12-04 09:34:38 +0100

    vanilla solution

--- src/main/java/org/demo/ChatController.java
+++ src/main/java/org/demo/ChatController.java
@@ -1,9 +1,16 @@
 package org.demo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sap.ai.sdk.orchestration.Message;
 import com.sap.ai.sdk.orchestration.OrchestrationClient;
 import com.sap.ai.sdk.orchestration.OrchestrationModuleConfig;
 import com.sap.ai.sdk.orchestration.OrchestrationPrompt;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.io.entity.StringEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Nonnull;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/chat")
@@ -28,7 +36,69 @@ public class ChatController {
 
   @GetMapping(value = "/step1", produces = "text/html")
   @Nonnull
-  String completion(@RequestParam("text") String text) {
+  String completionVanilla(@RequestParam("text") String text) throws IOException {
+    String HOST =  null;
+    String DEPLOYMENT = null;
+    String RESOURCE_GROUP = null;
+    String AUTH_TOKEN = null;
+
+    // Create HttpClient
+    try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+      String url = "https://%s/v2/inference/deployments/%s/completion".formatted(HOST, DEPLOYMENT);
+      HttpPost httpPost = new HttpPost(url);
+      httpPost.setHeader("Content-Type", "application/json; charset=UTF-8");
+      httpPost.setHeader("AI-Resource-Group", RESOURCE_GROUP);
+      httpPost.setHeader("Authorization", "Bearer " + AUTH_TOKEN);
+
+      String jsonPayload = """
+          {
+            "orchestration_config": {
+              "module_configurations": {
+                "llm_module_config": {
+                  "model_name":"gpt-4o",
+                  "model_params":{
+                    "temperature":0.5
+                  },
+                  "model_version":"latest"
+                },
+                "templating_module_config":{
+                  "template":[
+                    {"role":"system","content":"Render your response with HTML, use <p> tag to separate sentences.\\nUse <b>, <i> and <u> to emphasize words.\\nTry to imitate a German accent throughout the response.\\nLimit your response to 5 sentences."},
+                    {"role":"user","content":"What's the difference between Java and JavaScript?"}
+                  ]
+                }
+              },
+              "stream":false
+            },
+            "input_params":{},
+            "messages_history":[]
+          }
+          """;
+      StringEntity entity = new StringEntity(jsonPayload, ContentType.APPLICATION_JSON);
+      httpPost.setEntity(entity);
+
+      return httpClient.execute(httpPost, response ->
+          new ObjectMapper()
+              .readTree(response.getEntity().getContent())
+              .get("orchestration_result")
+              .get("choices").get(0)
+              .get("message")
+              .get("content")
+              .asText());
+    }
+  }
+
+  @GetMapping(value = "/step2", produces = "text/html")
+  @Nonnull
+  String completionLowLevel(@RequestParam("text") String text) {
+    var prompt = new OrchestrationPrompt(Message.system(systemMessage), Message.user(text));
+    var result = client.chatCompletion(prompt, config);
+    return result.getContent();
+  }
+
+  @GetMapping(value = "/step3", produces = "text/html")
+  @Nonnull
+  String completionHighLevel(@RequestParam("text") String text) {
     var prompt = new OrchestrationPrompt(Message.system(systemMessage), Message.user(text));
     var result = client.chatCompletion(prompt, config);
     return result.getContent();
